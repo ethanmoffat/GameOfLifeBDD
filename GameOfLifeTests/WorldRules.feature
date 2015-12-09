acdf62c0623b2e9535676de081de670243341cdf
Ethan Moffat <ethanmoffat@hotmail.com>
2015-12-08 16:45:26 -0800

    Specflow Scenarios
    Scenarios written in BDD language. Tests generated.

---
+++ GameOfLifeTests/WorldRules.feature
@@ -0,0 +1,40 @@
+﻿Feature: WorldRules
+   In order to simulate a world of cells
+   As a scientist interested in silly games
+   I want to know the discrete rules of the world
+
+Scenario: Live cells die due to underpopulation
+   Given a live cell
+   And the cell has less than 2 live neighbors
+   When I get the next generation of the world
+   Then the cell should be dead
+
+Scenario: Live cells with 2 neighbors live on to next generation
+   Given a live cell
+   And the cell has 2 live neighbors
+   When I get the next generation of the world
+   Then the cell should be alive
+
+Scenario: Live cells with 3 neighbors live on to next generation
+   Given a live cell
+   And the cell has 3 live neighbors
+   When I get the next generation of the world
+   Then the cell should be alive
+
+Scenario: Live cells die due to overpopulation
+   Given a live cell
+   And the cell has greater than 3 live neighbors
+   When I get the next generation of the world
+   Then the cell should be dead
+
+Scenario: Dead cells come back to life
+   Given a dead cell
+   And the cell has 3 live neighbors
+   When I get the next generation of the world
+   Then the cell should be alive
+
+Scenario: Dead cells stay dead
+   Given a dead cell
+   And the dead cell does not have exactly 3 live neighbors
+   When I get the next generation of the world
+   Then the cell should be dead
---
+++ GameOfLifeTests/WorldRulesSteps.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace GameOfLifeTests
+{
+    [Binding]
+    public class WorldRulesSteps
+    {
+        [Given(@"a live cell")]
+        public void GivenALiveCell()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the cell has less than (.*) live neighbors")]
+        public void GivenTheCellHasLessThanLiveNeighbors(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the cell has (.*) live neighbors")]
+        public void GivenTheCellHasLiveNeighbors(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the cell has greater than (.*) live neighbors")]
+        public void GivenTheCellHasGreaterThanLiveNeighbors(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"a dead cell")]
+        public void GivenADeadCell()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the dead cell does not have exactly (.*) live neighbors")]
+        public void GivenTheDeadCellDoesNotHaveExactlyLiveNeighbors(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I get the next generation of the world")]
+        public void WhenIGetTheNextGenerationOfTheWorld()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the cell should be dead")]
+        public void ThenTheCellShouldBeDead()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the cell should be alive")]
+        public void ThenTheCellShouldBeAlive()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
