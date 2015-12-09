3ffa17dbee1c7d9a845d843428e0ab545b31cae4
Ethan Moffat <ethanmoffat@hotmail.com>
2015-12-09 13:06:52 -0800

    UI Workflow Scenarios
    Added scenarios for the UI workflow. Includes information about changing
    state from setup of the world, running the simulation, pause/resume,
    reset, viewing previous generations of the world, and stopping the
    simulation if all cells are dead.

---
+++ GameOfLifeUITests/UIWorkflow.feature
@@ -0,0 +1,48 @@
+﻿Feature: UIWorkflow
+   In order to simulate the Game of Life
+   As a science enthusiast who enjoys silly games
+   I want to be able to easily see the Game of Life world as a simulation runs
+
+# "The background is run before each of your scenarios but after any of your Before Hooks."
+# See https://github.com/cucumber/cucumber/wiki/Background
+Background: 
+   Given I have started the application
+
+Scenario: Seed a world with live cells
+   Given The simulation is not running
+   When I add a seed cell to the world
+   Then The cell is reflected in the display of the world
+
+Scenario: Run the simulation
+   Given The simulation is not running
+   And I have a world that is seeded with at least one live cell
+   When I run the game of life simulation
+   Then The simulation enters the running state
+
+Scenario: Pause the simulation
+   Given The simulation is running
+   When I pause the simulation
+   Then The simulation enters the paused state
+
+Scenario: Resume the simulation
+   Given The simulation is paused
+   When I resume the simulation
+   Then The simulation enters the running state
+
+Scenario: Reset the simulation
+   Given The simulation is paused
+   When I reset the world
+   Then The simulation enters the initial state
+      And The world is set to the initial state
+
+Scenario: View previous generations
+   Given The simulation is paused
+   When I select a previous world generation
+   Then The previous generation is displayed
+
+Scenario: Simulation stops when all cells are dead
+   Given The simulation is running
+   When The current generation has no live cells
+      And The previous generation has at least one live cell
+   Then The simulation enters the initial state
+      And The world is set to the initial state
---
+++ GameOfLifeUITests/UIWorkflowSteps.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace GameOfLifeUITests
+{
+    [Binding]
+    public class UIWorkflowSteps
+    {
+        [Given(@"I have started the application")]
+        public void GivenIHaveStartedTheApplication()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"The simulation is not running")]
+        public void GivenTheSimulationIsNotRunning()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have a world that is seeded with at least one live cell")]
+        public void GivenIHaveAWorldThatIsSeededWithAtLeastOneLiveCell()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"The simulation is running")]
+        public void GivenTheSimulationIsRunning()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"The simulation is paused")]
+        public void GivenTheSimulationIsPaused()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I add a seed cell to the world")]
+        public void WhenIAddASeedCellToTheWorld()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I run the game of life simulation")]
+        public void WhenIRunTheGameOfLifeSimulation()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I pause the simulation")]
+        public void WhenIPauseTheSimulation()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I resume the simulation")]
+        public void WhenIResumeTheSimulation()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I reset the world")]
+        public void WhenIResetTheWorld()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I select a previous world generation")]
+        public void WhenISelectAPreviousWorldGeneration()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"The current generation has no live cells")]
+        public void WhenTheCurrentGenerationHasNoLiveCells()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"The previous generation has at least one live cell")]
+        public void WhenThePreviousGenerationHasAtLeastOneLiveCell()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"The cell is reflected in the display of the world")]
+        public void ThenTheCellIsReflectedInTheDisplayOfTheWorld()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"The simulation enters the running state")]
+        public void ThenTheSimulationEntersTheRunningState()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"The simulation enters the paused state")]
+        public void ThenTheSimulationEntersThePausedState()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"The simulation enters the initial state")]
+        public void ThenTheSimulationEntersTheInitialState()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"The world is set to the initial state")]
+        public void ThenTheWorldIsSetToTheInitialState()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"The previous generation is displayed")]
+        public void ThenThePreviousGenerationIsDisplayed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
