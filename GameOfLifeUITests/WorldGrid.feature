10641d0a8015bd54aefc71f68289cbcf9af9710a
Ethan Moffat <ethanmoffat@hotmail.com>
2015-12-11 13:00:37 -0800

    Added Spec for World Grid in UI
    Spec added for the WorldGrid control. Hopefully captured enough of the
    user-facing functionality, and kept it UI-agnostic enough so that if the
    interface changed the specification would still hold. 'White' added
    again via NuGet

---
+++ GameOfLifeUITests/WorldGrid.feature
@@ -0,0 +1,60 @@
+﻿Feature: WorldGrid
+   In order to simulate the game of life
+   As a game of life enthusiast
+   I want to have an easy way to seed and view the world
+
+Background:
+   Given The application has started
+
+Scenario: Add a cell to the world
+   Given I have a world with a live cell at 1, 1 displayed
+   When I select the cell at 1, 2
+   Then the cell should display as "alive"
+
+Scenario: Remove a cell from the world
+   Given I have a world with a live cell at 1, 1 displayed
+   When I select the cell at 1, 1
+   Then the cell should display as "dead"
+
+Scenario: Grid becomes un-editable when running
+   Given I have a world with a live cell at 1, 1 displayed
+     And I have a world with a live cell at 1, 2 displayed
+     And I have a world with a live cell at 1, 3 displayed
+   When I run the simulation in the ui
+   Then the world should not be editable
+
+Scenario: Grid becomes editable when paused
+   Given I have a world with a live cell at 1, 1 displayed
+     And I have a world with a live cell at 1, 2 displayed
+     And I have a world with a live cell at 1, 3 displayed
+     And the simulation has started
+   When I pause the simulation in the ui
+   Then the world should be editable
+
+Scenario: Grid becomes un-editable when resumed
+   Given I have a world with a live cell at 1, 1 displayed
+     And I have a world with a live cell at 1, 2 displayed
+     And I have a world with a live cell at 1, 3 displayed
+     And the simulation has started
+     And the simulation has been paused
+   When I resume the simulation in the ui
+   Then the world should not be editable
+
+Scenario: Grid becomes editable and cleared when reset
+   Given I have a world with a live cell at 1, 1 displayed
+     And I have a world with a live cell at 1, 2 displayed
+     And I have a world with a live cell at 1, 3 displayed
+     And the simulation has started
+     And the simulation has been paused
+   When I reset the simulation in the ui
+   Then the world should be editable
+    And the world should be reset
+
+Scenario: Grid becomes editable and cleared when all cells die
+   Given I have a world with a live cell at 1, 2 displayed
+     And I have a world with a live cell at 1, 3 displayed
+   When I run the simulation in the ui
+    And I let the simulation run
+   Then the simulation should stop when there are no live cells
+    And the world should be editable
+    And the world should be reset
---
+++ GameOfLifeUITests/WorldGridSteps.cs
@@ -0,0 +1,119 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using GameOfLifeUI;
+using TechTalk.SpecFlow;
+using TestStack.White;
+using TestStack.White.UIItems.WindowItems;
+
+namespace GameOfLifeUITests
+{
+   [Binding]
+   public class WorldGridSteps
+   {
+      private static Application _app;
+      private static Window _window;
+
+      [AfterScenario]
+      public static void AfterScenario()
+      {
+         Thread.Sleep(1000);
+         if (_app != null)
+            _app.Kill();
+      }
+
+      [Given(@"The application has started")]
+      public void GivenTheApplicationHasStarted()
+      {
+         //runs at beginning of each scenario
+         var currentDir = Directory.GetCurrentDirectory();
+
+         _app = Application.AttachOrLaunch(new ProcessStartInfo(Path.Combine(currentDir, "GameOfLifeUI.exe")));
+         _window = _app.GetWindow(MainForm.TITLE_TEXT);
+      }
+
+      [Given(@"I have a world with a live cell at (.*), (.*) displayed")]
+      public void GivenIHaveAWorldWithALiveCellAtDisplayed(int p0, int p1)
+      {
+         //_window.Get<>()
+      }
+
+      [Given(@"the simulation has started")]
+      public void GivenTheSimulationHasStarted()
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [Given(@"the simulation has been paused")]
+      public void GivenTheSimulationHasBeenPaused()
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [When(@"I select the cell at (.*), (.*)")]
+      public void WhenISelectTheCellAt(int p0, int p1)
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [When(@"I run the simulation in the ui")]
+      public void WhenIRunTheSimulationInTheUi()
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [When(@"I pause the simulation in the ui")]
+      public void WhenIPauseTheSimulationInTheUi()
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [When(@"I resume the simulation in the ui")]
+      public void WhenIResumeTheSimulationInTheUi()
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [When(@"I reset the simulation in the ui")]
+      public void WhenIResetTheSimulationInTheUi()
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [When(@"I let the simulation run")]
+      public void WhenILetTheSimulationRun()
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [Then(@"the cell should display as ""(.*)""")]
+      public void ThenTheCellShouldDisplayAs(string p0)
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [Then(@"the world should not be editable")]
+      public void ThenTheWorldShouldNotBeEditable()
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [Then(@"the world should be editable")]
+      public void ThenTheWorldShouldBeEditable()
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [Then(@"the world should be reset")]
+      public void ThenTheWorldShouldBeReset()
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [Then(@"the simulation should stop when there are no live cells")]
+      public void ThenTheSimulationShouldStopWhenThereAreNoLiveCells()
+      {
+         ScenarioContext.Current.Pending();
+      }
+   }
+}
