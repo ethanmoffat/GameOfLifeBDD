4725826d6881923e89694969c06e4c705829a468
Ethan Moffat <ethanmoffat@hotmail.com>
2015-12-11 14:41:43 -0800

    Basic UI automation
    Added in some basic automation. Unfortunately I'm not sure how to make
    the custom grid an automation peer, but it appears that the tests spec'd
    by BDD work well with UI automation in White.

--- GameOfLifeUITests/WorldGridSteps.cs
+++ GameOfLifeUITests/WorldGridSteps.cs
@@ -1,9 +1,12 @@
 ﻿using System.Diagnostics;
 using System.IO;
 using System.Threading;
+using System.Windows;
 using GameOfLifeUI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TechTalk.SpecFlow;
 using TestStack.White;
+using TestStack.White.UIItems;
 using TestStack.White.UIItems.WindowItems;
 
 namespace GameOfLifeUITests
@@ -17,7 +20,6 @@ namespace GameOfLifeUITests
       [AfterScenario]
       public static void AfterScenario()
       {
-         Thread.Sleep(1000);
          if (_app != null)
             _app.Kill();
       }
@@ -33,62 +35,69 @@ namespace GameOfLifeUITests
       }
 
       [Given(@"I have a world with a live cell at (.*), (.*) displayed")]
-      public void GivenIHaveAWorldWithALiveCellAtDisplayed(int p0, int p1)
+      public void GivenIHaveAWorldWithALiveCellAtDisplayed(int x, int y)
       {
-         //_window.Get<>()
+         ClickCellAt(x, y);
       }
 
       [Given(@"the simulation has started")]
       public void GivenTheSimulationHasStarted()
       {
-         ScenarioContext.Current.Pending();
+         RunSimulation();
       }
 
       [Given(@"the simulation has been paused")]
       public void GivenTheSimulationHasBeenPaused()
       {
-         ScenarioContext.Current.Pending();
+         PauseSimulation();
       }
 
       [When(@"I select the cell at (.*), (.*)")]
-      public void WhenISelectTheCellAt(int p0, int p1)
+      public void WhenISelectTheCellAt(int x, int y)
       {
-         ScenarioContext.Current.Pending();
+         ClickCellAt(x, y);
       }
 
       [When(@"I run the simulation in the ui")]
       public void WhenIRunTheSimulationInTheUi()
       {
-         ScenarioContext.Current.Pending();
+         RunSimulation();
       }
 
       [When(@"I pause the simulation in the ui")]
       public void WhenIPauseTheSimulationInTheUi()
       {
-         ScenarioContext.Current.Pending();
+         PauseSimulation();
       }
 
       [When(@"I resume the simulation in the ui")]
       public void WhenIResumeTheSimulationInTheUi()
       {
-         ScenarioContext.Current.Pending();
+         ResumeSimulation();
       }
 
       [When(@"I reset the simulation in the ui")]
       public void WhenIResetTheSimulationInTheUi()
       {
-         ScenarioContext.Current.Pending();
+         ResetSimulation();
       }
 
       [When(@"I let the simulation run")]
       public void WhenILetTheSimulationRun()
       {
-         ScenarioContext.Current.Pending();
+         //Would be nice to have a better method of waiting...this works for now
+         Thread.Sleep(1000);
       }
 
       [Then(@"the cell should display as ""(.*)""")]
-      public void ThenTheCellShouldDisplayAs(string p0)
+      public void ThenTheCellShouldDisplayAs(string deadOrAlive)
       {
+         //this is where we need the grid provider...
+         //bool alive = deadOrAlive.ToLower() == "alive";
+         //bool dead = deadOrAlive.ToLower() == "dead";
+
+         //_window.???
+
          ScenarioContext.Current.Pending();
       }
 
@@ -113,7 +122,47 @@ namespace GameOfLifeUITests
       [Then(@"the simulation should stop when there are no live cells")]
       public void ThenTheSimulationShouldStopWhenThereAreNoLiveCells()
       {
-         ScenarioContext.Current.Pending();
+         //there should be a better way to check this, instead of observing enabled state
+         //also, it throws an exception when getting a button that is not Visible
+         var reset = _window.Get<Button>("ResetButton");
+         var run = _window.Get<Button>("RunButton");
+         var resume = _window.Get<Button>("ResumeButton");
+         Assert.IsTrue(reset.Enabled);
+         Assert.IsTrue(resume.Enabled);
+         Assert.IsFalse(run.Enabled);
+      }
+
+      private static void ClickCellAt(int x, int y)
+      {
+         //would love to have a provider for the custom grid, but manually clicking it seems to work fine
+         var topRight = _window.Location;
+         topRight = new Point(topRight.X + 25, topRight.Y + 46);
+         _window.Mouse.Location = new Point(topRight.X + x * 17, topRight.Y + y * 17);
+         _window.Mouse.Click();
+      }
+
+      private static void RunSimulation()
+      {
+         var run = _window.Get<Button>("RunButton");
+         run.Click();
+      }
+
+      private static void PauseSimulation()
+      {
+         var pause = _window.Get<Button>("PauseButton");
+         pause.Click();
+      }
+
+      private static void ResumeSimulation()
+      {
+         var run = _window.Get<Button>("ResumeButton");
+         run.Click();
+      }
+
+      private static void ResetSimulation()
+      {
+         var pause = _window.Get<Button>("ResetButton");
+         pause.Click();
       }
    }
 }
